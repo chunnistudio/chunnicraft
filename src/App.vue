c3ba1bdc696725e726a75a2ef5e10a9ed2dd725d
Harpuia <zetsibou.harpuia@gmail.com>
2025-09-22 00:04:38 -0300

    sitio temporal

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <h1>Chunnicraft - Cobblemon</h1>
+  </header>
+
+  <main>
+    <h2>Sitio en construcción</h2>
+    <p>IP: cobblemon.chunni.studio</p>
+    <p>Version: 1.21.1</p>
+    <p>Plataforma de mods: <a href="https://modrinth.com/" target="_blank" rel="noopener">Modrinth</a></p>
+    <p>Modpack: <a href="/Cobblemon%20Chunni%20V6.mrpack" target="_blank" rel="noopener">Chunnicraft</a></p>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
