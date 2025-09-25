aaaa158a16f681f7260effaf1eef2fda923f5d78
Harpuia <zetsibou.harpuia@gmail.com>
2025-09-25 02:20:38 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  const archivoUrl = '/Cobblemon%20Chunni%20V6.mrpack'
+  const archivoUrl = '/Cobblemon%20Chunni%201.0.0.mrpack'
 </script>
 
 <template>
@@ -12,7 +12,7 @@
     <p>IP: cobblemon.chunni.studio</p>
     <p>Version: 1.21.1</p>
     <p>Plataforma de mods: <a href="https://modrinth.com/" target="_blank" rel="noopener">Modrinth</a></p>
-    <p>Modpack: <a :href="archivoUrl" target="_blank" rel="noopener">Chunnicraft</a></p>
+    <p>Modpack Versión 1.0.0: <a :href="archivoUrl" target="_blank" rel="noopener">Chunnicraft</a></p>
   </main>
 </template>
 
