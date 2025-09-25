17423eea166527a8ea140026ac37091a8ff8c04b
Harpuia <zetsibou.harpuia@gmail.com>
2025-09-25 02:27:10 -0300

    change

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
 
   <main>
     <h2>Sitio en construcción</h2>
-    <p>IP: cobblemon.chunni.studio</p>
+    <p>IP: mc.chunni.studio</p>
     <p>Version: 1.21.1</p>
     <p>Plataforma de mods: <a href="https://modrinth.com/" target="_blank" rel="noopener">Modrinth</a></p>
     <p>Modpack Versión 1.0.0: <a :href="archivoUrl" target="_blank" rel="noopener">Chunnicraft</a></p>
