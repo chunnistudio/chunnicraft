965d852789f046d3b59a62158b7281a3f3f3e2c0
Harpuia <zetsibou.harpuia@gmail.com>
2025-11-08 20:36:01 -0300

    feat: se actualiza a version 1.3, se añade link a mapa

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  const archivoUrl = '/Cobblemon%20Chunni%201.1-fix.mrpack'
+  const archivoUrl = '/Cobblemon%20Chunni%201.3.mrpack'
 </script>
 
 <template>
@@ -12,7 +12,8 @@
     <p>IP: mc.chunni.studio</p>
     <p>Version: 1.21.1</p>
     <p>Plataforma de mods: <a href="https://modrinth.com/" target="_blank" rel="noopener">Modrinth</a></p>
-    <p>Modpack Versión 1.1-fix: <a :href="archivoUrl" target="_blank" rel="noopener">Chunnicraft</a></p>
+    <p>Modpack Versión 1.3: <a :href="archivoUrl" target="_blank" rel="noopener">Chunnicraft</a></p>
+    <p>Mapa del server: <a href="https://mcmap.chunni.studio/" target="_blank" rel="noopener">Ver Mapa</a></p>
   </main>
 </template>
 
