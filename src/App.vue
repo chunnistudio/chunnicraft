d81e4fb1e9a8876be1acda2ad9c722c719d4e142
Harpuia <zetsibou.harpuia@gmail.com>
2025-11-12 22:11:15 -0300

    version 1.3.2

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,7 @@
 
       <p>Version: 1.21.1</p>
       <p>Plataforma de mods: <a href="https://modrinth.com/" target="_blank" rel="noopener">Modrinth</a></p>
-      <p>Modpack Versión 1.3: <a :href="archivoUrl" target="_blank" rel="noopener" style="cursor:pointer">Chunnicraft</a></p>
+      <p>Modpack Versión 1.3.2: <a :href="archivoUrl" target="_blank" rel="noopener" style="cursor:pointer">Chunnicraft</a></p>
       <p>Mapa del server: <a href="https://mcmap.chunni.studio/" target="_blank" rel="noopener">Ver Mapa</a></p>
 
     </div>
