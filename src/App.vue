29a69f859ae985d770b5b472412ebe28984d0b3c
Harpuia <marcelo.cardenas@usach.cl>
2025-12-27 22:05:07 -0300

    Update App.vue
    fix: se cambia version a 1.5.0 en texto

--- src/App.vue
+++ src/App.vue
@@ -51,7 +51,7 @@ let resourcepacks = ref([])
 
       <p>Version de Minecraft: 1.21.1</p>
       <p>Plataforma de mods: <a href="https://modrinth.com/" target="_blank" rel="noopener">Modrinth</a></p>
-      <p>Modpack Versión 1.4.5: <a :href="archivoUrl" target="_blank" rel="noopener" style="cursor:pointer">Chunnicraft</a></p>
+      <p>Modpack Versión 1.5.0: <a :href="archivoUrl" target="_blank" rel="noopener" style="cursor:pointer">Chunnicraft</a></p>
       <p>Mapa del server: <a href="https://mcmap.chunni.studio/" target="_blank" rel="noopener">Ver Mapa</a></p>
     </div>
 
